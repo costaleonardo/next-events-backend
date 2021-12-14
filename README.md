5ca59b2ed068c563666a854e93e302566512d70b
Leonardo da Costa <costaleonardo97@gmail.com>
2021-12-14 02:58:21 -0500

    Set up Cloudinary with Strapi.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Strapi application
 
 A quick description of your strapi application
+
+Start server - npm run developer || yarn develop
--- config/server.js
+++ config/server.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cloudinary_name: env('CLOUDINARY_NAME', 'dhroaqswh'),
+  cloudinary_key: env('CLOUDINARY_KEY', '858278631173778'),
+  cloudinary_secret: env('CLOUDINARY_SECRET', 'tR_piWRfwYq9lFsZDCiAGYGJyeE')
 });
